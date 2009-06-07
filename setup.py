ce800e2320a99a37b6b7550a85ae8fe393263ff0
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2009-06-07 14:39:08 +0000

    prepare release

--- setup.py
+++ setup.py
@@ -30,12 +30,6 @@ setup(name='osha.surveyanswers',
       ],
       entry_points="""
       # -*- Entry points: -*-
-
-      [distutils.setup_keywords]
-      paster_plugins = setuptools.dist:assert_string_list
-
-      [egg_info.writers]
-      paster_plugins.txt = setuptools.command.egg_info:write_arg
       """,
       paster_plugins=["ZopeSkel"],
       )
