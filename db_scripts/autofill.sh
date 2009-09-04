946363ccd399695f21f9bc3342eaf6d33dff1f67
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2009-09-04 10:23:56 +0000

    Better database name :-)

--- db_scripts/autofill.sh
+++ db_scripts/autofill.sh
@@ -1,34 +1,34 @@
 #delete db
 #in buildout, run make_db.py
 #in buildout, run sqlite3 db < db_setup.sql
-rm ~/projects/osha_git/buildout/beeer
+rm ~/projects/osha_git/buildout/surveyanswers
 echo .
 python make_db.py
 echo .
-sqlite3 ~/projects/osha_git/buildout/beeer < db_setup.sql
+sqlite3 ~/projects/osha_git/buildout/surveyanswers < db_setup.sql
 echo .
 python questions.py > questions.sql
 echo .
-sqlite3 ~/projects/osha_git/buildout/beeer < questions.sql
+sqlite3 ~/projects/osha_git/buildout/surveyanswers < questions.sql
 echo .
 ./bin/python questions2.py > questions2.sql
 echo .
-sqlite3 ~/projects/osha_git/buildout/beeer < questions2.sql
+sqlite3 ~/projects/osha_git/buildout/surveyanswers < questions2.sql
 echo .
 python groups.py > groups.sql
 echo .
-sqlite3 ~/projects/osha_git/buildout/beeer < groups.sql
+sqlite3 ~/projects/osha_git/buildout/surveyanswers < groups.sql
 echo .
-sqlite3 ~/projects/osha_git/buildout/beeer < step1.sql > step1.txt
+sqlite3 ~/projects/osha_git/buildout/surveyanswers < step1.sql > step1.txt
 echo .
 python step2.py > step2.sql
 echo .
-sqlite3 ~/projects/osha_git/buildout/beeer < step2.sql > step2.txt
+sqlite3 ~/projects/osha_git/buildout/surveyanswers < step2.sql > step2.txt
 echo .
 python step3.py > step3.sql
 echo .
-sqlite3 ~/projects/osha_git/buildout/beeer < step3.sql
+sqlite3 ~/projects/osha_git/buildout/surveyanswers < step3.sql
 echo .
-sqlite3 ~/projects/osha_git/buildout/beeer < special.sql
+sqlite3 ~/projects/osha_git/buildout/surveyanswers < special.sql
 echo .
 
