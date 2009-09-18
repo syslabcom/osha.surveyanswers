b8effaa4c7037e1094c25af1fd94aa1128e1ea21
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2009-09-18 15:05:35 +0000

    Back to development: 1.4.1dev

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.4.0'
+version = '1.4.1dev'
 
 long_description = (
     read('README.txt')
@@ -63,6 +63,5 @@ setup(name='osha.surveyanswers',
       entry_points="""
       # -*- entry_points -*- 
       """,
-      paster_plugins = ["ZopeSkel"],
       )      
       
