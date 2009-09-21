7453d0b6c70529bd33bf55bc40bbb7e7203d6d08
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2009-09-21 14:55:02 +0000

    Updated the categories to which to group to

--- db_scripts/special.sql
+++ db_scripts/special.sql
@@ -1,9 +1,10 @@
-insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'size_5'), 1, 'Size 1', 1);
-insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'size_5'), 2, 'Size 2', 2);
-insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'size_5'), 4, 'Size 3', 3);
-insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'size_5'), 8, 'Size 4', 4);
-insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'sec3'), 1, 'Sector 1', 1);
-insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'sec3'), 2, 'Sector 2', 2);
-insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'sec3'), 4, 'Sector 3', 3);
+insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'size_5'), 1, '10 to 19 employees', 1);
+insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'size_5'), 2, '20 to 49 employees', 2);
+insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'size_5'), 4, '50 to 249 employees', 3);
+insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'size_5'), 8, '250 to 499 employees', 4);
+insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'size_5'), 16, '500 or more employees', 5);
+insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'sec3'), 1, 'Production sector', 1);
+insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'sec3'), 2, 'Private Services', 2);
+insert into answer_meanings(question_id, answer_bit, answer_text, position) values ((select id from questions where question_field = 'sec3'), 4, 'Public Services', 3);
 update questions set question = 'Company Size' where question_field = 'size_5';
 update questions set question = 'Sector Type' where question_field = 'sec3';
