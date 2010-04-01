ccc3ff57a95ea03de96503419c287555f69f03e9
thomas <thomas@97d050b9-b799-4550-a901-4f04598d4601>
2010-04-01 16:32:45 +0000

    custom language selector for ESENER: preserve the question/country, and preserve the grouping

--- osha/surveyanswers/browser/viewlets.py
+++ osha/surveyanswers/browser/viewlets.py
@@ -1,5 +1,8 @@
 from osha.theme.browser.viewlets import OSHALanguageSelector
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.i18n.locales.browser.selector import LanguageSelector
+from zope.component import getMultiAdapter
+from Products.LinguaPlone.interfaces import ITranslatable
 
 class ESENERLanguageSelector(OSHALanguageSelector):
     """ Override LinguaPlone's language selector to provide our own template
@@ -16,10 +19,24 @@ class ESENERLanguageSelector(OSHALanguageSelector):
         subsite_path = oshaview.subsiteRootPath()
         potential_subsite = self.context.restrictedTraverse(subsite_path)
             
+        # get the potentially present question- and country-id
+        # Due to apache rewriting, we cannot use PATH_INFO, but must contruct the path manually
+        # first, get the URL and snip the SERVER_URL off
+        path = self.request.get('URL')[len(self.request.get('SERVER_URL'))+1:]
+        # then pre-pend VirtualURL components if present
+        elems = [x for x in self.request.get('VirtualRootPhysicalPath', [''])] + path.split('/')
+        # join it to a path
+        virtual_path = '/'.join(elems)
+        # get the context's path
+        context_path = '/'.join(self.context.getPhysicalPath())
+        # just an extra test, as the following expression should always be true
+        if virtual_path.startswith(context_path):
+            question_path = virtual_path[len(context_path):]
+        else:
+            question_path = ''
 
-        for data in results:
-            data['url'] = self.context.absolute_url()+'/switchLanguage?set_language='+data['code']
-        return results
+        group_by = 'group_by' in self.request and '&group_by=' + \
+            self.request.get('group_by') or ''
 
         # for translatable content, directly link to the translated objects
         translatable = ITranslatable(self.context, None)
@@ -27,11 +44,6 @@ class ESENERLanguageSelector(OSHALanguageSelector):
             translations = translatable.getTranslations()
         else:
             translations = []
-        links =  self.context.getBRefs('lingualink')
-        # create a dict that maps language to LinguaLink object
-        lang_to_link = dict()
-        for link in links:
-            lang_to_link[link.Language()] = link
 
         for data in results:
             data['translated'] = data['code'] in translations
@@ -39,9 +51,8 @@ class ESENERLanguageSelector(OSHALanguageSelector):
                 trans = translations[data['code']][0]
                 state = getMultiAdapter((trans, self.request),
                         name='plone_context_state')
-                data['url'] = state.view_url() + '?set_language=' + data['code']
-            elif data['code'] in lang_to_link:
-                data['url'] = lang_to_link[data['code']].absolute_url()
+                data['url'] = state.view_url() + question_path + \
+                    '?set_language=' + data['code'] + group_by
             else:
                 state = getMultiAdapter((self.context, self.request),
                         name='plone_context_state')
